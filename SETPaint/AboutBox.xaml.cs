a0136c842aaf3cf227764d016237f6479fdbfbfb
Jacob Huras <jacobdhuras@gmail.com>
2017-01-25 23:44:09 -0500

    project base commit
    entire solution: 2 projects and packages

---
+++ SETPaint/AboutBox.xaml.cs
@@ -0,0 +1,38 @@
+﻿/*
+* FILE : AboutBox.xaml.cs
+* PROJECT : PROG2120 - Final Project
+* PROGRAMMER : Jacob Huras, Anthony Bastos
+* FIRST VERSION : 2016-12-13
+* DESCRIPTION :
+* The About Box is used to display information related to the
+* SETPaint application
+*/
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SETPaint
+{
+    /// <summary>
+    /// Interaction logic for AboutBox.xaml
+    /// </summary>
+    public partial class AboutBox : Window
+    {
+        public AboutBox()
+        {
+            InitializeComponent();
+        }
+    }
+}
